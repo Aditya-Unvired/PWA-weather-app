4e00d9f57c7ecefa016477e7fd79744ac0edb1a5
Aditya_Patel_developer <aditya.patel@unvired.io>
2022-10-21 13:28:55 +0530

    changed lstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,11 +46,12 @@ export class AppComponent {
     console.log('called for data')
     this._weatherService.getWeather(this.form.controls['city'].value, this.form?.controls['country'].value).subscribe((data: any) => 
     {
+      console.log('called')
         if (data?.cod === "200") {
-    console.log('res',data)
-
-          this.weather = data
+          this.weather = data;
+          localStorage.setItem('data',JSON.stringify(this.weather))
         } else if (!data) {
+      console.log('!data')
           this.weather = localStorage.getItem('data');
           this.weather = JSON.parse(this.weather)
         }
